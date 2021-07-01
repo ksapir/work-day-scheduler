06840e674ed968c45690f35b24af3c7f455897e4
Karen Pion <karenlpion@gmail.com>
2021-07-01 18:02:27 -0400

    time-blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,27 @@
 var dayDisplayEl = $("#current-day");
+var containerEl = $('#time-block');
+var hour9 = $('#9');
+var hour10 = $('#10');
+var hour11 = $('#11');
+var hour12 = $('#12');
+var hour13 = $('#13');
+var hour14 = $('#14');
+var hour15 = $('#15');
+var hour16 = $('#16');
+var hour17 = $('#17');
+
+// var hourArray = [
+//     hour9,
+//     hour10,
+//     hour11,
+//     hour12,
+//     hour13,
+//     hour14,
+//     hour15,
+//     hour16,
+//     hour17,
+// ]
+
 
 // Displaying date
 function displayDate() {
@@ -6,9 +29,6 @@ function displayDate() {
     dayDisplayEl.text(today);
 }
 displayDate()
-
-
-// Create time blocks for 9am-5pm
 
 // Color code time blocks based on whether passed, current or future
 
