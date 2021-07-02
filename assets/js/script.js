82259b5561dfdf639c032aa7e8069089ce2de841
Karen Pion <karenlpion@gmail.com>
2021-07-02 00:47:14 -0400

    adding color-coded time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var dayDisplayEl = $("#current-day");
 var containerEl = $('#time-block');
+var textArea = $('.input')
+
 var hour9 = $('#9');
 var hour10 = $('#10');
 var hour11 = $('#11');
@@ -8,31 +10,31 @@ var hour13 = $('#13');
 var hour14 = $('#14');
 var hour15 = $('#15');
 var hour16 = $('#16');
-var hour17 = $('#17');
+var hour17 = $('#17')
 
-// var hourArray = [
-//     hour9,
-//     hour10,
-//     hour11,
-//     hour12,
-//     hour13,
-//     hour14,
-//     hour15,
-//     hour16,
-//     hour17,
-// ]
+var currentTime = moment().format('H');
+var hourArray = [9,10,11,12,13,14,15,16,17]
+
 
 
 // Displaying date
 function displayDate() {
-    var today = moment().format("MMMM Do, YYYY");
+    var today = moment().format("dddd, MMMM Do, YYYY");
     dayDisplayEl.text(today);
 }
 displayDate()
 
+
 // Color code time blocks based on whether passed, current or future
 
-// Create the ability for user input in time blocks
+    for (var i = 0; i < hourArray.length; i++) {
+        if (currentTime> hourArray[i]){
+            $(".color-block").addClass("past")
+        } else if (currentTime<hourArray[i]){
+            $(".color-block").addClass("future")
+        } else if (currentTime === hourArray[i]){
+            $(".color-block").addClass("present")
+        }
+    }
 
 // Create ability to save text in each time block to local storage even when page is refreshed
-
